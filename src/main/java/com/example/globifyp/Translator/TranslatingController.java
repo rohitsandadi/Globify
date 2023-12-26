89bce040d94550804ca862a272246f01690c450a
RohitS606 <rohitsandadi@gmail.com>
2023-12-26 15:55:05 -0800

    finished basic web service

---
+++ src/main/java/com/example/globifyp/Translator/TranslatingController.java
@@ -0,0 +1,38 @@
+package com.example.globifyp.Translator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TranslatingController {
+
+    @Autowired
+    Translating mytranslating;
+
+    @PostMapping("/translator/translating")
+    public String translateInput1(@RequestParam(value = "text", defaultValue = "No text found") String text, @RequestParam(value = "language", defaultValue = "en-US") String language) throws Exception{
+        String result = "dead";
+        /*try{
+            result = mytranslating.textTranslator(text, language);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }*/
+
+        System.out.println(result);
+        return result;
+    }
+    @GetMapping("/translator/translating")
+    public String translateInput2(@RequestParam(value = "text", defaultValue = "No text found") String text, @RequestParam(value = "language", defaultValue = "en-US") String language) throws Exception{
+        String result = "dead";
+        /*try{
+            result = mytranslating.textTranslator(text, language);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }*/
+
+        System.out.println(result);
+        return result;
+    }
+}
